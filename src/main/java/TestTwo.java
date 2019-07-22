60432cb4316c2222e126f81d222feaf4a20651ee
thatman6666 <306650605@qq.com>
2019-07-22 20:32:06 +0800

    this is a testTwo Git

---
+++ src/main/java/TestTwo.java
@@ -0,0 +1,10 @@
+/**
+ * @description:this is a testTwo
+ * @author: xujiangxi
+ * @create: 2019-07-22 20:29:50
+ */
+public class TestTwo {
+    public static void main(String[] args) {
+        System.out.println("xixixiix");
+    }
+}
