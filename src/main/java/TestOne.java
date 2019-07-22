931537dce4d252e6e967ccba1fb99ccacbf22a5f
thatman6666 <306650605@qq.com>
2019-07-22 20:13:52 +0800

    this is a test Git

---
+++ src/main/java/TestOne.java
@@ -0,0 +1,10 @@
+/**
+ * @description:do some test things
+ * @author: xujiangxi
+ * @create: 2019-07-22 20:11:17
+ */
+public class TestOne {
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
